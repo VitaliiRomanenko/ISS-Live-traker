60863298bb34076d564375d0755a73788687dc9c
VitaliiRomanenko <vitaliinizhynromanenko@gmail.com>
2021-11-20 18:31:22 +0200

    ILT-16 add map styles list

---
+++ src/utils/mapStyles.js
@@ -0,0 +1,12 @@
+const MAP_STYLES = {
+    streets: "mapbox://styles/mapbox/streets-v11",
+    outdoors: "mapbox://styles/mapbox/outdoors-v11",
+    light: "mapbox://styles/mapbox/light-v10",
+    dark: "mapbox://styles/mapbox/dark-v10",
+    satellite: "mapbox://styles/mapbox/satellite-v9",
+    satellite_streets: "mapbox://styles/mapbox/satellite-streets-v11",
+    navigation_day: "mapbox://styles/mapbox/navigation-day-v1",
+    navigation_night: "mapbox://styles/mapbox/navigation-night-v1"
+}
+
+export default MAP_STYLES
