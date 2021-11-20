fe62576ae3ca22b163df9f1d9104b1597a4141c2
VitaliiRomanenko <vitaliinizhynromanenko@gmail.com>
2021-11-20 14:44:05 +0200

    ILT-15 Make SideBar close func

--- src/components/Map/MapSideBar/SideBar.js
+++ src/components/Map/MapSideBar/SideBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from "./SideBar.module.css"
 const SideBar = ({visible, setVisible}) => {
-    //for future
     return (
-        <div className={(visible? styles.visible + " ": "") + styles.sideBar}>
-            <button onClick={()=>setVisible(false)}>feef</button>
+        <div
+            className={`${styles.wrapper} ${visible? styles.wrapperVisible: ""}`}
+            id={"sidebarWrapper"}
+            onClick={e=> {if(e.target.id === "sidebarWrapper") setVisible(false)}}
+        >
+            <div className={`${styles.sideBar} ${visible? styles.sideBarVisible: ""}`}>
+                <button onClick={()=>setVisible(false)}>close</button>
+            </div>
         </div>
     );
-};
+}
 
 export default SideBar;
